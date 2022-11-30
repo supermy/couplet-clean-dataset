38f4637ba332adc5a9c2c133091c820b277d7709
supermy <springclick@gmail.com>
2022-11-30 20:37:35 +0800

    Update build-txt.sh
    数据增加[CLS] and [SEP] 标志。

--- build-txt.sh
+++ build-txt.sh
@@ -1,15 +1,11 @@
-# 2022-11-29
-paste -d'。 ' couplets/train/in.txt  couplets/train/out.txt > couplet-train.txt
-paste -d'。 ' couplets/test/in.txt  couplets/test/out.txt > couplet-test.txt
-# sed -i '' '1i \
-# text\
-# ' couplet-train.txt
-# sed -i '' '1i \
-# text\
-# ' couplet-test.txt
+# 2022-11-29 for linux, not mac 
+paste -d'@' couplets/train/in.txt  couplets/train/out.txt > couplet-train.txt
+paste -d'@' couplets/test/in.txt  couplets/test/out.txt > couplet-test.txt
+sed -i -e "s/^/[CLS]&/g;s/@/[SEP]/g" couplet-train.txt
+sed -i -e "s/^/[CLS]&/g;s/@/[SEP]/g" couplet-test.txt
 
-sed -i '' 's/[[:space:]]//g' couplet-train.txt
-sed -i '' 's/[[:space:]]//g' couplet-test.txt
+sed -i 's/[[:space:]]//g' couplet-train.txt
+sed -i 's/[[:space:]]//g' couplet-test.txt
 
 awk '{if (length(max)<length()) max=$0}END{print length(max)}'  couplets/train/in.txt
 awk '{if (length(max)<length()) max=$0}END{print length(max)}'  couplets/test/in.txt
