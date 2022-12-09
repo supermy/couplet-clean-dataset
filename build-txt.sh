d6c5ff98ef21c8ca297394ff0fc12c27215474b4
supermy <springclick@gmail.com>
2022-12-09 10:11:53 +0800

    Update build-txt.sh

--- build-txt.sh
+++ build-txt.sh
@@ -1,8 +1,10 @@
+# 2022-12-09 GPT2格式CLM:[CLS]line1[SEP][CLS]line2[SEP]
 # 2022-11-29 for linux, not mac 
+
 paste -d'@' couplets/train/in.txt  couplets/train/out.txt > couplet-train.txt
 paste -d'@' couplets/test/in.txt  couplets/test/out.txt > couplet-test.txt
-sed -i -e "s/^/[CLS]&/g;s/@/[SEP]/g" couplet-train.txt
-sed -i -e "s/^/[CLS]&/g;s/@/[SEP]/g" couplet-test.txt
+sed -i -e "s/^/[CLS]&/g;s/$/&[SEP]/g;s/@/[SEP][CLS]/g" couplet-train.txt
+sed -i -e "s/^/[CLS]&/g;s/$/&[SEP]/g;s/@/[SEP][CLS]/g" couplet-test.txt
 
 sed -i 's/[[:space:]]//g' couplet-train.txt
 sed -i 's/[[:space:]]//g' couplet-test.txt
